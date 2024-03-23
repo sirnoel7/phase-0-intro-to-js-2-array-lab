79f99582258bcf8d8b0cbc809285e9c6e382af43
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-03-23 23:03:42 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield" ]
+
+function destructivelyAppendCat (name) {
+    cats.push("Ralph");
+    
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  function appendCat(name) {
+    return cats.concat(["Broom"]);
+  }
+  
+  function prependCat(name) {
+    return [name, ...cats];
+  }
+  
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+  
+  
